33e52184514948c540b7012cb5eb627cf5df98e1
Aki Gotoh <agotoh@enjoylab.co.jp>
2024-08-04 09:39:09 +0900

    投稿ページテンプレート

---
+++ single.php
@@ -0,0 +1,36 @@
+<?php
+/*
+ *
+ * Template Name: 投稿ページテンプレート
+ *
+ */
+?>
+<?php get_header(); ?>
+<div class="container-xxl">
+<main>
+    <?php if(have_posts()):while(have_posts()):the_post(); ?>
+      <?php
+        $cat = get_the_category();
+        $catname = $cat[0]->cat_name;
+      ?>
+      <article>
+        <h1 class="article-title">
+          <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+        </h1>
+        <ul class="meta">
+          <li><?php the_time('Y/m/d'); ?></li>
+          <li><?php echo $catname; ?></li>
+        </ul>
+        <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail(); ?></a>
+        <div class="text"><?php the_content(); ?></div>
+      </article>
+    <?php endwhile;endif; ?>
+
+    <ul class="post-link">
+      <li><?php previous_post_link('%link', '< 前の記事へ'); ?></li>
+      <li><?php next_post_link('%link', '次の記事へ >'); ?></li>
+    </ul>
+  </main>
+</div>
+<div class="container-fluid footerImgContainer"></div>
+<?php get_footer(); ?>
